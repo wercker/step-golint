799909e1e581cc8e87161dd7bb2ccbf28375ab74
Naoki AINOYA <ainonic@gmail.com>
2015-08-13 03:38:39 +0900

    make lint thresholds configurable

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+THRESHOLD_WARN=${WERCKER_GOLINT_THRESHOLD_WARN-5}
+THRESHOLD_FAIL=${WERCKER_GOLINT_THRESHOLD_FAIL-10}
+
 if [ -n "$WERCKER_GOLINT_EXCLUDE" ]; then
   LINTLINES=$("$WERCKER_STEP_ROOT"/golint ./... | grep -ve "$WERCKER_GOLINT_EXCLUDE" | tee lint_results.txt | wc -l | tr -d " ")
 else
@@ -7,6 +10,6 @@ else
 fi
 
 cat lint_results.txt
-if [ "$LINTLINES" -ge 10 ]; then echo "Time to tidy up: $LINTLINES lint warnings." > "$WERCKER_REPORT_MESSAGE_FILE"; fail "Time to tidy up."; fi
-if [ "$LINTLINES" -ge 5 ]; then echo "You should be tidying soon: $LINTLINES lint warnings." > "$WERCKER_REPORT_MESSAGE_FILE"; warn "You should be tidying soon."; fi
+if [ "$LINTLINES" -ge "${THRESHOLD_FAIL}" ]; then echo "Time to tidy up: $LINTLINES lint warnings." > "$WERCKER_REPORT_MESSAGE_FILE"; fail "Time to tidy up."; fi
+if [ "$LINTLINES" -ge "${THRESHOLD_WARN}" ]; then echo "You should be tidying soon: $LINTLINES lint warnings." > "$WERCKER_REPORT_MESSAGE_FILE"; warn "You should be tidying soon."; fi
 if [ "$LINTLINES" -gt 0 ]; then echo "You are fairly tidy: $LINTLINES lint warnings." > "$WERCKER_REPORT_MESSAGE_FILE"; fi
